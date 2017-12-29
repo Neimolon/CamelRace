95befeb443b9aeda16228fc7404a7edb1ecd1f03
diego <diego@vault>
2017-12-29 11:49:56 +0100

    Añadido Bullets

--- main.js
+++ main.js
@@ -117,7 +117,46 @@ Player.update = function(){
     return pack;
 }
 
+var Bullet = function(angle){
+    var self = Entity();
+    self.id = Math.random();
+    self.spdX = Math.cos(angle/180*Math.PI) * 10;
+    self.spdY = Math.sin(angle/180*Math.PI) * 10;
+    
+    self.timer = 0;
+    self.toRemove = false;
+    
+    var super_update = self.update;
+    self.update = function(){
+        if(self.timer++ >100)
+            self.toRemove = true;
+        super_update();
+    }
+    
+    Bullet.list[self.id] = self;
+    return self;
+}
 
+Bullet.list = {};    
+
+Bullet.update = function(){
+    if(Math.random() < 0.1){
+        Bullet(Math.random() * 360);
+    }
+    
+    var pack = [];
+    
+    for(var i in Bullet.list){
+        var bullet = Bullet.list[i];
+        bullet.update();
+        pack.push({
+            x:bullet.x,
+            y:bullet.y
+        });
+    }
+    
+    return pack;
+}
 
 //Connections
 var io = require("socket.io")(serv,{});
@@ -139,7 +178,10 @@ io.sockets.on("connection", function(socket){
 //Loop    
 setInterval(function(){
    // console.log("tick");
-    var pack = Player.update();
+    var pack = {
+        player: Player.update(),
+        bullet: Bullet.update()
+    } 
     
     
     for(var i in SOCKET_LIST){
