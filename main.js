4112d9a46d76dd9791fd23a6d4576f89df930a45
diego <diego@vault>
2017-12-29 18:15:34 +0100

    New Login System available!
    Notes:
    - Added mongojs package
    - Login system just works...but insecurely, passwords still not encrypted on database
    - Added a file (mongo-db-creation) where can find brief structure

--- main.js
+++ main.js
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 
+var mongojs = require("mongojs");
+var db = mongojs("localhost:27017/CamelRace", ["account", "progress"]);
+
 var express = require("express");
 var app = express();
 var serv = require("http").Server(app);
@@ -193,7 +196,34 @@ Bullet.update = function(){
 //   !!!!!!!  WARNING OF DEATH OR WORSE !!!!!!!!!
 //   !ALLWAYS SET "DEBUG" TO FALSE ON PRODUCTION!
 //   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-var DEBUG = true;
+//var DEBUG = true;
+var DEBUG = false;
+
+
+var isValidPassword = function(data,cb){
+    db.account.find( {username:data.username,password:data.password} , function(err,res){
+        if(res.length > 0)
+            cb(true);
+        else
+            cb(false);
+    });
+        
+}
+
+var isUsernameTaken = function(data,cb){
+    db.account.find( {username:data.username} , function(err,res){
+        if(res.length > 0) 
+            cb(true);
+        else
+            cb(false);
+    });  
+}
+
+var addUser = function(data,cb){
+    db.account.insert( {username:data.username,password:data.password} , function(err,res){
+        cb(false);
+    });  
+}
 
 
 //Connections
@@ -204,8 +234,34 @@ io.sockets.on("connection", function(socket){
     socket.id = Math.random(); 
     SOCKET_LIST[socket.id] = socket;
     
-    Player.onConnect(socket);
- 
+    
+
+    socket.on("signIn", function(data){
+        
+        isValidPassword(data, function(res){
+            if(res){
+                Player.onConnect(socket);
+                socket.emit("signInResponse",{success: true});               
+            }else{
+                socket.emit("signInResponse",{success: false});
+            }
+        });
+
+        
+    });
+    socket.on("signUp", function(data){
+        isUsernameTaken(data, function(res){
+            if(res){
+                socket.emit("signupResponse",{success: false});
+            }else{
+                addUser(data, function(){
+                    socket.emit("signTrueResponse",{success: false});
+                });
+
+            }
+        });
+    });
+    
     socket.on("disconnect", function(){
         delete SOCKET_LIST[socket.id];
         Player.onDisconnect(socket);
